0b70ec0f78ceab0eaedfba5e799c82c66e2f13f3
Leon Higgin <admin@leonmichaelhiggin>
2020-08-18 17:28:49 +0100

    First firebase call

---
+++ Assets/Firebase/InitialiseFirebase.cs
@@ -0,0 +1,16 @@
+﻿using Firebase.Analytics;
+using UnityEngine;
+
+namespace Firebase
+{
+    public class InitialiseFirebase : MonoBehaviour
+    {
+        private void Start()
+        {
+            FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+            {
+                FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+            });
+        }
+    }
+}
