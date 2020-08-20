ad310cba36f931946e025775352efccbe7acecc8
Leon Higgin <admin@leonmichaelhiggin>
2020-08-20 11:04:23 +0100

    Create a static firebase manager so we can access and send events anywhere

---
+++ Assets/Firebase/FireBaseManager.cs
@@ -0,0 +1,46 @@
+﻿using Firebase.Analytics;
+using UnityEngine;
+
+namespace Firebase
+{
+    public static class FireBaseManager
+    {
+        private static FirebaseApp app; //Usages?
+        private static bool fireBaseActive = false;
+    
+        public static void InitialiseFirebase()
+        {
+            FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+            {
+                var dependencyStatus = task.Result;
+                
+                if (dependencyStatus == DependencyStatus.Available) 
+                {
+                    Debug.Log($"Firebase active");
+                    FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+                    
+                    app = FirebaseApp.DefaultInstance;
+                    
+                    fireBaseActive = true;
+                    
+                } 
+                else 
+                {
+                    // Firebase Unity SDK is not safe to use here.
+                    Debug.LogError($"Could not resolve all Firebase dependencies: {dependencyStatus}. Firebase inactive");
+                    app = null;
+                    fireBaseActive = false;
+                }
+            });
+        }
+
+        // Update is called once per frame
+        public static void SetPlayerIdentification()
+        {
+            if (!fireBaseActive) return;
+            
+            FirebaseAnalytics.SetUserId("test");
+            FirebaseAnalytics.SetUserProperty("deviceId", SystemInfo.deviceUniqueIdentifier);
+        }
+    }
+}
---
+++ Assets/Firebase/FirebaseTestScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Firebase
+{
+    public class FirebaseTestScript : MonoBehaviour
+    {
+        private void Awake()
+        {
+            FireBaseManager.InitialiseFirebase();
+        }
+        
+        private void Start()
+        {
+            FireBaseManager.SetPlayerIdentification();
+        }
+
+        private void Update()
+        {
+            if (Input.touches.Length > 0)
+            {
+                FireBaseManager.SetPlayerIdentification();
+            }
+        }
+    }
+}
